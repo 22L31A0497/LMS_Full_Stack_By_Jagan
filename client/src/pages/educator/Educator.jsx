1ebd3ce4d0a1b77d0f69f8b69cddaa3ed1bec159
22L31A0497 <jaganmohanraokuna2004@gmail.com>
2025-02-16 22:54:02 +0530

    Routes total completed and along with the nav bar component 30% completed today

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom' // add Outlet
 
 const Educator = () => {
   return (
     <div>
       <h1>Educator page</h1>
+      <div>
+         {<Outlet/>}
+      </div>
     </div>
   )
 }
