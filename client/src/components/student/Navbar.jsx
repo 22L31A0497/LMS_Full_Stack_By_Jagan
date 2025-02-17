0516fdccaf52fcb57bf200f790c82fd4656906c1
22L31A0497 <jaganmohanraokuna2004@gmail.com>
2025-02-17 23:18:20 +0530

    Created NavBar component for all pages and created signIn SignUp forms by using the Clerk and also implemented Hero Component in Home page

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import { assets } from '../../assets/assets';
 import { Link, useLocation } from 'react-router-dom';
+import { useClerk } from '@clerk/clerk-react';
+import { UserButton,useUser } from '@clerk/clerk-react';  
 
 const Navbar = () => {
   const location = useLocation(); // ✅ Fix: Use useLocation hook
   const isCourseListPage = location.pathname.includes('/course-list');
+  const { user } = useUser(); // ✅ Fix: Use useUser hook
+  const{openSignIn} = useClerk(); // ✅ Fix: Use useClerk hook
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
       <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
-        <div>
-          <button>Become Educator</button>
-          | <Link to='/my-enrollments'>My Enrollments</Link>  
-        </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
-      </div>
+        <div className='flex items-center gap-5'>
+          { user &&
+            <>
+              <button>Become Educator</button>
+          |      <Link to='/my-enrollments'>My Enrollments</Link>  
+            </>
+           }
+
+          </div>{ user ? <UserButton /> :
+           <button 
+           onClick={() => openSignIn()} 
+            className="bg-blue-600 text-white px-5 py-2 rounded-full transition duration-300 hover:bg-blue-700 cursor-pointer">Create Account</button>
+           }
+          </div>
+       {/* For Phone Screens */}
+      <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:txt-xs'>
+        { user &&
+            <>
+              <button>Become Educator</button>
+          |      <Link to='/my-enrollments'>My Enrollments</Link>  
+            </>
+           }
+            </div> 
+            {
+              user ? <UserButton /> :
+    
+             <button onClick={()=> openSignIn()}><img src={assets.user_icon} alt=""/></button>
+            }</div>
     </div>
   );
 };
