fb8d6837ed07e1ecc7cb9fff5e4528bf797e40d9
22L31A0497 <jaganmohanraokuna2004@gmail.com>
2025-02-21 22:07:56 +0530

    CourseCard section completed and also make it as dynamically fetching all the details instead of dummy  daata and naavigating to home on by clicking the edemy logo ...etc did today

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -10,9 +10,9 @@ const CoursesSection = () => {
     <div className='py-16 md:px-40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
       <p className='text-sm md:text-base text-gray-500 mt-3'>
-        Discover our top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.
+        Discover our top-rated courses across various categories. From coding and design to <br/>business and wellness, our courses are crafted to deliver results.
       </p>
-      <div>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] mt-8 md:px-0 md:my-16 y-10 gap-4"      >
         {allCourses.length > 0 ? (
           allCourses.slice(0, 4).map((course, index) => <CourseCard key={index} course={course} />)
         ) : (
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,19 +1,31 @@
 import React, { useState, useEffect, createContext } from 'react';
 import { dummyCourses } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const currency = import.meta.env.VITE_CURRENCY;
+  const navigate =useNavigate();
   const [allCourses, setAllCourses] = useState([]);
+  const[isEducator,setIsEducator] = useState(true);
 
   // Load courses when the component mounts
+   const calculateRating =(course)=>{
+    if(course.courseRatings.length === 0) return 0;
+    let totalRating = 0;
+    course.courseRatings.forEach(rating =>totalRating += rating.rating);
+
+    return totalRating / course.courseRatings.length;
+   }
+
+
   useEffect(() => {
     setAllCourses(dummyCourses);
   }, []); // ✅ Directly setting the courses
 
   return (
-    <AppContext.Provider value={{ currency, allCourses }}>
+    <AppContext.Provider value={{ currency, allCourses,navigate,calculateRating,  isEducator,setIsEducator}}>
       {children}
     </AppContext.Provider>
   );
