a1c4b286b8a1e309397382f9e8ceb8bac106e4d8
22L31A0497 <jaganmohanraokuna2004@gmail.com>
2025-02-27 21:35:37 +0530

    Home_page_of LMS is completed

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -8,14 +8,31 @@ const Footer = () => {
         <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30'>
           <div className='flex flex-col md:items-start items-center w-full'>
             <img src={assets.logo_dark} alt="logo"/>
-            <p className="text-gray-500 text-sm">Committed to providing quality education and resources to help you grow and succeed.
+            <p className="mt-6 text-center md:text-left text-sm text-sm text-white/80">Committed to providing quality education and resources to help you grow and succeed.
              </p>
           </div>
-          <div></div>
-          <div></div>
+          <div className='flex flex-col md:items-start items-center w-full'>
+            <h2 className='font-semibold text-white mb-5'>Company</h2>
+            <ul className='flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
+              <li> <a href='#'>Home</a></li>
+              <li> <a href='#'>About Us</a></li>
+              <li> <a href='#'>Contact Us</a></li>
+              <li> <a href='#'>Privacy Policy</a></li>
+              <li> <a href='#'>Home</a></li>
+            </ul>
+          </div>
+          <div className='hidden md:flex flex-col items-start w-full'>
+            <h2 className='font-semibold text-white mb-5'>Subscibe to our newsletter</h2>
+            <p className='text-sm text-white/80'>The latest new, articles and resources, sent to your inbox weekly.
+            </p>
+            <div className='flex items-center gap-2 pt-4'>
+              <input type='email' placeholder='Enter your email' className='border border-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm'/>
+              <button className='bg-blue-600 w-24 h-9 text-white rounded'>Subscribe</button>
+            </div>
+          </div>
 
         </div>
-        <p>Copyright 2025 © Edemy-By Jagan. All rights reserved. Unauthorized reproduction or distribution is strictly prohibited</p>
+        <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2025 © Edemy-By Jagan. All rights reserved.</p>
       </footer>
     </div>
   )
