c6368a69af65aff64c03ae07805625c44b6ab463
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-06-01 22:48:53 +0530

    Completed Frontend part

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom' // add Outlet
+import Navbar from '../../components/educator/Navbar.jsx' // import Navbar
+import Sidebar from '../../components/educator/Sidebar.jsx'
+import { assets } from '../../assets/assets.js' // import assets
+import Footer from '../../components/educator/Footer.jsx'
 
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator page</h1>
-      <div>
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar /> {/* Use Navbar component */}
+      <div className='flex'>
+        <Sidebar/>
+        <div className='flex-1'>
          {<Outlet/>}
+         </div>
       </div>
+      <Footer/>
     </div>
   )
 }
