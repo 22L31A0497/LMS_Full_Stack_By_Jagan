7e8a280b4d6aeb6e054d356852dd9305186f038a
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-06-02 22:05:24 +0530

    corrected

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -20,7 +20,6 @@ export const clerkWebhooks = async (req, res) => {
                 _id: data.id,
                 email: data.email_addresses[0].email_address,
                 name: data.first_name + " " + data.last_name,
-                 // Ensure you handle passwords securely
                 imageUrl: data.image_url,
                 
             }
